9f98354ae176ec0aa3d5c41273b37f11cdb73734
William Morgan <billm@wamusa.com>
2022-10-11 12:03:10 -0500

    Changed Excludes to regex

--- nerdle.py
+++ nerdle.py
@@ -7,7 +7,8 @@ from tokenize import String
 from turtle import numinput
 from unicodedata import digit
 
-def nerdle():
+
+def findEquation():
     # this 7 deep nested loop may look very inefficient but at this point, there are a maximum of about 60,000 possible total combinations
     # after processing the seed equations and most likely less than 5,000 combinations are left.
     global possibleSolution
@@ -18,59 +19,63 @@ def nerdle():
             equation = first + second
             for third in digits[2]:
                 equation = first + second + third
-                if not any(exclude in equation for exclude in excludes):
+                if (not re.search(r"[\+\-\*\/]{2}", equation)):
                     for forth in digits[3]:
                         equation = first + second + third + forth
-                        if not any(exclude in equation for exclude in excludes):
+                        if (not re.search(r"[\+\-\*\/]{2}", equation)):
                             for fifth in digits[4]:
                                 equation = first + second + third + forth + fifth
-                                if not any(exclude in equation for exclude in excludes):
+                                if (not re.search(r"[\+\-\*\/]{2}", equation)):
                                     for sixth in digits[5]:
                                         equation = first + second + third + forth + fifth + sixth
-                                        if not any(exclude in equation for exclude in excludes):
+                                        if (not re.search(r"[\+\-\*\/]{2}", equation)):
                                             for seventh in digits[6]:
                                                 equation = first + second + third + forth + fifth + sixth + seventh
-                                                if not any(exclude in equation for exclude in excludes):
-                                                        for eighth in digits[7]:
-                                                                equation = first + second + third + forth + fifth + sixth + seventh + eighth
-                                                                if not any(exclude in equation for exclude in excludes):
-                                                                    if (validate(equation)):
-                                                                        possibleSolution = True
-                                                                        return getResult(equation)
+                                                if (not re.search(r"[\+\-\*\/]{2}", equation)):
+                                                    for eighth in digits[7]:
+                                                        equation = first + second + third + forth + fifth + sixth + seventh + eighth
+                                                        if (validate(equation)):
+                                                            possibleSolution = True
+                                                            return getResult(equation)
+
 
 def validate(equation):
     global includes
-    return all(required in equation for required in includes) and isNerdle(equation) and isMathle(equation)
+    return all(required in equation for required in includes) and isNerdle(equation)
+
 
 def isNerdle(equation):
-    global excludes
     equalPos = equation.find('=')
-    return ((len(equation) == 8) and 
+    return ((len(equation) == 8) and
             (all(eachChar in '0123456789+-*/=' for eachChar in equation)) and
-            (not any(exclude in equation for exclude in excludes)) and
-            (equalPos in [4,5,6]) and
+            (not re.search(r"[\+\-\*\/]{2}", equation)) and
+            (equalPos in [4, 5, 6]) and
             (equation.count('=') == 1) and
             (not any(eachChar in '+-*/' for eachChar in equation[equalPos + 1:])) and
             (any(eachChar in "+-*/" for eachChar in equation[:equalPos])) and
-            (not re.search("[-+*\/=]0\d", equation)))
+            (not re.search(r"[-+*\/=]0\d", equation)) and
+            (eval(equation[:equalPos]) == int(equation[equalPos + 1:])))
 
-def isMathle(equation):
-    equalPos = equation.find('=')
-    return (eval(equation[:equalPos]) == int(equation[equalPos + 1:]))
 
-def getResult(equation, prompts= ["      My Guess:  ", "  How did I do?  "]):
-    print (prompts[0] + equation)
+def removeOperators(index):
+    for operator in "+-*/=":
+        if (operator in digits[index]):
+            digits[index].remove(operator)
+
+
+def getResult(equation, prompts=["      My Guess:  ", "  How did I do?  "]):
+    print(prompts[0] + equation)
     result = input(prompts[1])
     if (result == '2' or result[:8] == '22222222'):
         return True
-    if (len(result) != 8 or not all(each in ['0', '1', '2'] for each in result) or result[equation.find('=')] == '0'):
-        if (len(result) == 8 and isNerdle(result) and isMathle(result)):
+    if (len(result) != 8 or not all(each in ['0', '1', '2'] for each in result)):
+        if (isNerdle(result)):
             return getResult(result, ["\n    Your Guess:  ", "How did you do?  "])
         else:
-            print ("\nI don't understand your input.\nPlease try again.")
+            print("\nI don't understand your input.\nPlease try again.")
             return getResult(equation, prompts)
     for each in '0123456789+-*/=':
-        foundOneOrTwo = False
+        foundOne = False
         foundZero = False
         index = equation.find(each)
         while (index >= 0):
@@ -78,31 +83,26 @@ def getResult(equation, prompts= ["      My Guess:  ", "  How did I do?  "]):
                 digits[index] = [each]
                 foundZero = False
                 if (each in "+-*/="):
-                    for operator in "+-*/=":
-                        if (operator in digits[index - 1]):
-                            digits[index - 1].remove(operator)
-                        if (operator in digits[index + 1]):
-                            digits[index + 1].remove(operator)
+                    removeOperators(index - 1)
+                    removeOperators(index + 1)
             else:
                 if(each in digits[index]):
                     digits[index].remove(each)
                 if (result[index] == '1' and not each in includes):
                     includes.append(each)
             foundZero = foundZero or result[index] == '0'
-            foundOneOrTwo = foundOneOrTwo or result[index] in '12'
-            nextIndex = equation[index + 1:].find(each)
-            if (nextIndex >= 0):
-                index += nextIndex + 1
-            else:
-                index = -1
-        if (foundZero and not foundOneOrTwo):
+            foundOne = foundOne or result[index] == '1'
+            index = equation.find(each, index + 1)
+        if (foundZero and not foundOne):
             for digit in digits:
                 if (len(digit) > 1 and each in digit):
                     digit.remove(each)
     return (False)
 
+
 def randomizeOptimizeDigits():
-    prioritiesList = ['/','*','-','+','1','2','3','4','5','6','7','8','9','0']
+    prioritiesList = ['/', '*', '-', '+', '1', '2',
+                      '3', '4', '5', '6', '7', '8', '9', '0']
     for digit in digits:
         random.shuffle(prioritiesList)
         for prioity in prioritiesList:
@@ -110,12 +110,13 @@ def randomizeOptimizeDigits():
                 digit.remove(prioity)
                 digit.insert(0, prioity)
 
-excludes = ['++', '+-', '+*', '+/', '+=', '-+', '--', '-*', '-/', '-=', '*+', '*-', '**', '*/', '*=', '/+', '/-', '/*', '//', '/=', '/0']
+
 includes = []
-digits = [['9','8','7','6','5','4','3','2','1'], ['+','-','*','/','9','8','7','6','5','4','3','2','1','0'], ['+','-','*','/','9','8','7','6','5','4','3','2','1','0'], ['+','-','*','/','9','8','7','6','5','4','3','2','1','0'], ['+','-','*','/','9','8','7','6','5','4','3','2','1','0'], ['=','9','8','7','6','5','4','3','2','1','0'], ['=','9','8','7','6','5','4','3','2','1','0'], ['9','8','7','6','5','4','3','2','1','0']]
+digits = [['9', '8', '7', '6', '5', '4', '3', '2', '1'], ['+', '-', '*', '/', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0'], ['+', '-', '*', '/', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0'], ['+', '-', '*', '/', '9', '8', '7', '6', '5', '4', '3',
+                                                                                                                                                                                                          '2', '1', '0'], ['+', '-', '*', '/', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0'], ['=', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0'], ['=', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0'], ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0']]
 possibleSolution = True
 exclimation = "Woohoo!\n\n\n"
-print ("\n\n\nput a 0 below each black, 1 below each purple and 2 below each green. Or, enter a new Nerdle equation to test.\n")
+print("\n\n\nput a 0 below each black, 1 below each purple and 2 below each green. Or, enter a new Nerdle equation to test.\n")
 getResult("0+12/3=4")
 if (len(digits[6]) == 1):
     getResult("9*8-65=7")
@@ -123,11 +124,15 @@ else:
     getResult("9*8-7=65")
 if '=' not in digits[5] and '=' not in digits[6]:
     digits[4] = ['=']
+    for operator in "+-*/":
+        if (operator in digits[3]):
+            digits[3].remove(operator)
+        if (operator in digits[5]):
+            digits[5].remove(operator)
 randomizeOptimizeDigits()
-while (not nerdle()):
-    print()
+while (not findEquation()):
     if (not possibleSolution):
         exclimation = "There was an invalid input. No solution found!\n\n\n"
         break
     randomizeOptimizeDigits()
-print (exclimation)
+print(exclimation)
